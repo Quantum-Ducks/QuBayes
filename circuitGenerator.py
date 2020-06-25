cde780b456d26e4e5a62027b4d9ef6f8e96f788a
mwbryant <>
2020-06-25 15:45:21 -0400

    Gave up on oil, started on the simple 3 node graph, works up until first barrier, next I'll move to juypter (promise)

--- circuitGenerator.py
+++ circuitGenerator.py
@@ -6,29 +6,56 @@ from qiskit.providers.aer.noise import NoiseModel
 from qiskit.tools.visualization import plot_histogram
 from math import atan2, sqrt
 
-graph = {'IR': ([], [.75, .25]), # From 2004.14803, Fig 10
+basic_graph = { 'A': ([], [.2, .8]),
+                'B': ([], [.3, .7]),
+                'C': (['A','B'], [.15, .85, .3, .7, .4, .6, .1, .9])
+              }
+#ir = q4, oi = q3, sm = q2, sp = q0
+# why does the paper do this
+# XXX figure 11 is bad, too many magic numbers
+oil_graph = {'IR': ([], [.75, .25]), # From 2004.14803, Fig 10
          'SM': (['IR'], [.3, .7, .8, .2]), #P(0|!A), P(1|!A), P(0|A), P(1|A)
          'OI': ([], [.6, .4]),
          'SP': (['OI', 'SM'], [.9, .1, .5, .5, .4, .6, .2, .8])
         }
 
 def main():
-    qbits = num_qbits_needed(graph)
-    print("need: " + str(qbits) + " qbits")
-    
-    #circuit = QuantumCircuit(1,1)
-    #circuit.h(0)
-    #circuit.measure(0,0)
-    #run_circuit(circuit, "test")
+    circuit = create_circuit(basic_graph)
+    run_circuit(circuit, "test")
 
+# TODO auto generate the circuit
+def create_circuit(graph):
+    (qbits, cbits) = num_qbits_needed(graph)
+    qc = QuantumCircuit(qbits, cbits)
+    #XXX how to keep track as to what bits are used and what for
+    next_free_qbit = 0
+    #Loop and find the parentless nodes, assign their rotations first
+    for node in graph:
+        if len(graph[node][0])==0:
+            #Gross graph work
+            probs = graph[node][1]
+            print(probs)
+            qc.ry(angle_from_probability(probs[1],probs[0]), next_free_qbit)
+            next_free_qbit += 1
+    qc.barrier()
+    return qc
+
+# TODO Code section 3.2
+# XXX Can we generalize this to C^nY gates
+def add_ccy(circuit, qbits, ancillabits):
+    pass
+
+#XXX Gross
 def num_qbits_needed(graph):
-    counter = 0
+    qbits = 0
+    cbits = 0
     for state in graph:
-        counter += 1
+        qbits += 1
+        cbits += 1
         in_edges = len(graph[state][0])
-        if in_edges != 0:
-            counter += in_edges-1
-    return counter
+        if in_edges != 0: # count ancilla qbits
+            qbits += in_edges-1
+    return (qbits, cbits)
 
 #TODO assertAlmostEqual(angle_from_probability(.8,.2), 2.2143)
 def angle_from_probability(p1, p0):
