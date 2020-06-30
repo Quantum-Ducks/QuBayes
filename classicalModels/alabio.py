6ab60622fcb5c7b827fdd04c720a3e64e92f0de8
madhunt <madannahunt@gmail.com>
2020-06-30 15:00:32 -0400

    both models implemented except for probabilities

--- classicalModels/alabio.py
+++ classicalModels/alabio.py
@@ -13,15 +13,18 @@ res = N.InitNetica2_bn (env, mesg)
 print mesg
 
 # initializing the network with environment
-bayesian_network = N.NewNet_bn ("Mallard", env)
+bayesian_network = N.NewNet_bn ("Alabio", env)
 
 # create each node in the network
 StateMaskOrder  = N.NewNode_bn("StateMaskOrder", 2, bayesian_network)
 Testing = N.NewNode_bn("Testing", 2, bayesian_network)
 TravelToWork = N.NewNode_bn("TravelToWork", 4, bayesian_network)
 TravelToRec = N.NewNode_bn("TravelToRec", 4, bayesian_network)
+Race = N.NewNode_bn("Race", 4, bayesian_network)
+Poverty = N.NewNode_bn("Poverty", 4, bayesian_network)
 Cases = N.NewNode_bn("Cases", 4, bayesian_network)
 Age = N.NewNode_bn("Age", 2, bayesian_network)
+Health = N.NewNode_bn("Health", 4, bayesian_network)
 Deaths = N.NewNode_bn("Deaths", 2, bayesian_network)
 
 # adding states to each node
@@ -29,8 +32,11 @@ N.SetNodeStateNames_bn(StateMaskOrder, "no, yes")
 N.SetNodeStateNames_bn(Testing, "less5, great5")
 N.SetNodeStateNames_bn(TravelToWork, "majorDec, modDec, minorDec, inc")
 N.SetNodeStateNames_bn(TravelToRec, "majorDec, modDec, minorDec, inc")
+N.SetNodeStateNames_bn(Race, "less15, 15to30, 30to45, great45")
+N.SetNodeStateNames_bn(Poverty, "less11, 11to13, 13to15, great15")
 N.SetNodeStateNames_bn(Cases, "dec, minorInc, modInc, majorInc")
 N.SetNodeStateNames_bn(Age, "lessThanAvg, moreThanAvg")
+N.SetNodeStateNames_bn(Health, "rare, somewhat, common, very")
 N.SetNodeStateNames_bn(Deaths, "dec, inc")
 
 # adding links between nodes
@@ -38,8 +44,13 @@ N.AddLink_bn(StateMaskOrder, Cases)
 N.AddLink_bn(Testing, Cases)
 N.AddLink_bn(TravelToWork, Cases)
 N.AddLink_bn(TravelToRec, Cases)
+N.AddLink_bn(Race, Cases)
+N.AddLink_bn(Poverty, Cases)
 N.AddLink_bn(Cases, Deaths)
 N.AddLink_bn(Age, Deaths)
+N.AddLink_bn(Health, Deaths)
+N.AddLink_bn(Race, Deaths)
+N.AddLink_bn(Poverty, Deaths)
 
 # setting the probabilities for each top level node
 # set conditional probabilities
@@ -49,12 +60,15 @@ N.SetNodeProbs(StateMaskOrder, )
 N.SetNodeProbs(Testing, )
 N.SetNodeProbs(TravelToWork, )
 N.SetNodeProbs(TravelToRec, )
+N.SetNodeProbs(Race, )
+N.SetNodeProbs(Poverty, )
 
 # second layer (root)
 N.SetNodeProbs(Age, )
+N.SetNodeProbs(Health, )
 
 # second layer (child)
-N.SetNodeProbs(Cases, ) 
+N.SetNodeProbs(Cases, )
 
 # third layer (child)
 N.SetNodeProbs(Deaths, )
