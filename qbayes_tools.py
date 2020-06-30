1db8959474c23ace66026bb034739c058025ecbc
Margie B <mbruff@unc.edu>
2020-06-30 03:42:09 -0400

    added Node class to tools code

--- qbayes_tools.py
+++ qbayes_tools.py
@@ -21,3 +21,24 @@ def generate_cond_keys(s_0, s_i):
         cond_keys.append("%s|%s"%(str(enum[0]), parent_str))
         
     return cond_keys
+
+
+class Node:
+    # A single variable in the Bayesian network
+    def __init__(self, name, data, states=None, parents=[]):
+        ### INPUTS ###
+        # name:    str    name of variable
+        # data:    array  state data for the node
+        # states:  dict   keys are state names, values are the int each takes on in the data
+        # parents: list   strings of names of parent nodes to this node
+        ##############
+        
+        if states == None:
+            states = {}
+            for i in range(max(data) + 1):
+                states.update({str(i) : i})
+        
+        self.name = name
+        self.data = data
+        self.states = states
+        self.parents = parents
